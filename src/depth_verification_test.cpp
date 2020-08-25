e5059f4d0d23b91eb917e584aca3e2f83c50495e
Wang Pengfei <tslwpf@nus.edu.sg>
2020-08-25 09:34:43 +0800

    added depth verification

---
+++ src/depth_verification_test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "depth_verification.hpp"
+
+int main(){
+
+    std::string imgl, imgr;
+
+    std::ifstream f;
+    f.open("../depth_verification_data/images.txt");
+
+    f >> imgl >> imgr;
+
+    f.close();
+
+    std::vector<std::string> imageFileNames = {"../depth_verification_data/" + imgl, "../depth_verification_data/" + imgr};
+
+    DepthVerification dv;           
+    dv.parseYamlConfig("../data/t265-ds-none.yaml");
+    dv.readImageFiles(imageFileNames);
+    dv.start();
+
+
+    return 0;
+}
