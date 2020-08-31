fc37579660d1a1fed21613a16a3f96d1ec489ff5
Wang Pengfei <tslwpf@nus.edu.sg>
2020-08-31 23:06:41 +0800

    added support for omni and radtan distort

--- include/depth_verification.hpp
+++ include/depth_verification.hpp
@@ -1,20 +1,12 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <cassert>
 #include <string>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <sophus/se3.hpp>
 #include <basalt/camera/generic_camera.hpp>
-#include <pangolin/pangolin.h>
 #include <pangolin/display/image_view.h>
-#include <pangolin/gl/gltext.h>
-
-#include "calibration_parser.hpp"
 
 class DepthVerification{
 public:
@@ -25,264 +17,26 @@ public:
     void start();
 
 private:
-
-    struct Point{ float x, y; };
+    struct Point{ float x = -1, y = -1; };
 
     std::map<std::string,std::string> camera_type_map = {{"pinhole" , "pinhole"},
-                                                         {"ds"      , "ds"}};
+                                                         {"ds"      , "ds"},
+                                                         {"omni", "ucm"}};
 
     void OnSelectionCallback(pangolin::ImageViewHandler::OnSelectionEventData, int cam_id);
     void drawImageViewOverlay(pangolin::View& v, size_t cam_id);
     void findWorldPoint();
+    void undistort(int cam_id, std::vector<double> distortParams);
+
+    std::vector<basalt::GenericCamera<double>> cams_;
+    std::vector<cv::Mat> images;
+    std::vector<Point> points;
+    Eigen::Matrix<double, 4, 1> worldPoint;
+    Sophus::SE3<double> T_cn_cnm1;
 
     template <class Derived> 
     Eigen::Matrix<typename Derived::Scalar, 4, 1> triangulate(
         const Eigen::MatrixBase<Derived>& f0,
         const Eigen::MatrixBase<Derived>& f1,
         const Sophus::SE3<typename Derived::Scalar>& T_0_1);
-
-    std::vector<basalt::GenericCamera<double>> cams_;
-    // std::vector<MyCam> cams_;
-    Sophus::SE3<double> T_cn_cnm1;
-    std::vector<cv::Mat> images;
-    std::vector<Point> points;
-    Eigen::Matrix<double, 4, 1> worldPoint;
-
 };
-
-
-DepthVerification::DepthVerification() : points(2, {-1,-1}), worldPoint(Eigen::Vector4d::Zero()) {}
-
-// Parse camera model, intrinsic params, T_cn_cnm1
-// Distortion, Resolution and RosTopic not parsed
-void DepthVerification::parseYamlConfig(const std::string yaml_file){
-
-    std::cout << "Parsing Param File " << yaml_file << std::endl;
-
-    YAML::Node configs = YAML::LoadFile(yaml_file);
-    
-    for(const auto& config : configs)
-    {
-        // Parse camera model
-        std::string cameraModel = config.second["camera_model"].as<std::string>();
-        auto cam = basalt::GenericCamera<double>::fromString(cameraModel);
-        // MyCam cam;
-
-        // Parse intrinsic parameters
-        int intrinsicSize = config.second["intrinsics"].size();
-        // assert(cam.getN() == intrinsicSize);
-        Eigen::VectorXd intrinsicParam(intrinsicSize);  
-        for(int i=0; i<intrinsicSize; i++){
-            intrinsicParam((i+4) % intrinsicSize) = config.second["intrinsics"][i].as<double>();
-        }                                           
-        cam.applyInc(intrinsicParam);
-        // cam.setParam(intrinsicParam);
-
-        cams_.push_back(cam);
-
-        // Parse Exrinisic Calibration
-        if(config.second["T_cn_cnm1"]){
-            std::vector<Eigen::RowVector4d> T;
-            for (size_t i = 0; i < 4 ; i++)
-            {
-                T.emplace_back(config.second["T_cn_cnm1"][i].as<std::vector<double>>().data());
-            }
-            Eigen::Matrix4d M;
-            M <<    T[0], 
-                    T[1], 
-                    T[2], 
-                    T[3];
-            T_cn_cnm1 = Sophus::SE3d(M);
-        }
-
-        // std::cout << "Camera " <<  cams_.size() - 1 << " initialised, type = " << cam.getName() << std::endl ;
-        std::cout << "Intrinsics: " << cam.getParam().transpose() << std::endl;
-    }
-
-    std::cout << "Extrinsic (cam0 to cam1): " <<  std::endl << T_cn_cnm1.matrix() << std::endl << std::endl;
-
-}
-
-void DepthVerification::readImageFiles(const std::vector<std::string> filenames){
-
-    for(auto filename : filenames){
-        cv::Mat image = cv::imread(filename, cv::IMREAD_GRAYSCALE);
-        if(image.empty()){
-            std::cout << "Error loading image: " << filename << std::endl;
-            exit(1);
-        }
-        // std::cout << image.size() << std::endl;
-        images.push_back(image);
-    }
-}
-
-void DepthVerification::start(){
-
-    // int r = images[0].rows, c=images[0].cols;
-    // double x[] = {c/10, c/2, 9*c/10, c/2}; //left, up, right, down
-    // double y[] = {r/2, r/10, r/2, 9*r/10};
-    // for(int i=0; i<4; i++){
-    // Eigen::Matrix<double, 4, 1> worldPoint;
-    // Eigen::Matrix<double, 2, 1> a;
-    //     a = {x[i], y[i]};
-    //     cams_[1].unproject(a, worldPoint);
-    //     std::cout<<x[i] <<" "<< y[i]<<std::endl;
-    //     std::cout<<worldPoint.transpose()<<std::endl;
-    // }
-
-    pangolin::CreateWindowAndBind("Depth Verifier" , 1600, 900);
-    
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);         // for displaying text
-    
-    // this is big window
-    auto& img_view_display =
-        pangolin::CreateDisplay()
-            .SetBounds(0.2, 1.0, 0.0, 0.4)
-            .SetLayout(pangolin::LayoutEqual);
-
-    // this stores ptr to each image
-    std::vector<std::shared_ptr<pangolin::ImageView>> img_view;
-
-    // this is image format
-    pangolin::GlPixFormat fmt;
-    fmt.glformat = GL_LUMINANCE;
-    fmt.gltype = GL_UNSIGNED_BYTE;
-    fmt.scalable_internal_format = GL_LUMINANCE8;
-
-    for(int i=0; i<2; i++){
-
-        auto view = std::make_shared<pangolin::ImageView>();
-
-        view->SetImage(images[i].ptr<uchar>(0,0), images[i].cols, images[i].rows, images[i].step, fmt);
-        view->OnSelectionCallback = std::bind(&DepthVerification::OnSelectionCallback, this, std::placeholders::_1, i);
-        view->SetDrawFunction(std::bind(&DepthVerification::drawImageViewOverlay, this, std::placeholders::_1, i));
-    
-        img_view.emplace_back(view);            
-        img_view_display.AddDisplay(*view);
-        
-    }
-    
-    // pangolin::GlFont font(pangolin::AnonymousPro_ttf, 30);
-
-    while( !pangolin::ShouldQuit() )
-    {
-        // Clear screen and activate view to render into
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        img_view_display.Activate();
-
-        double dist = 1/worldPoint(3);
-
-        glColor3f(1.0, 1.0, 1.0);
-        std::string str = "unit vector (x,y,z): " + std::to_string(worldPoint(0)) + " " 
-                                                  + std::to_string(worldPoint(1)) + " " 
-                                                  + std::to_string(worldPoint(2)) + ", distance: " 
-                                                  + std::to_string(dist);
-        pangolin::GlFont::I().Text(str.c_str()).Draw(1000.0, 200.0);
-
-        str = "Actual coordinates (x,y,z): " + std::to_string(dist*worldPoint(0)) + " " 
-                                             + std::to_string(dist*worldPoint(1)) + " " 
-                                             + std::to_string(dist*worldPoint(2));
-        pangolin::GlFont::I().Text(str.c_str()).Draw(1000.0, 250.0);
-
-        // Swap frames and Process Events
-        pangolin::FinishFrame();
-    }
-}
-
-void DepthVerification::OnSelectionCallback(pangolin::ImageViewHandler::OnSelectionEventData e, int cam_id){
-    
-    if (!e.dragging){
-
-        e.handler.GetHover(points[cam_id].x, points[cam_id].y);
-                
-        findWorldPoint();    
-    }
-}
-
-// This function is run by Pangolin automatically for all viewports, set by SetDrawFunction()
-void DepthVerification::drawImageViewOverlay(pangolin::View& v, size_t cam_id)
-{
-    v.Activate();
-    
-    auto &pt = points[cam_id];
-
-    if(pt.x >= 0 && pt.y >= 0){
-        float color[] = {255, 0, 0};
-        glColor3fv(color);
-        pangolin::glDrawCircle(pt.x, pt.y, 2);    
-    }
-}
-
-void DepthVerification::findWorldPoint(){
-    for(auto point:points){
-        if(point.x < 0 || point.y < 0) {
-            worldPoint = Eigen::Matrix<double, 4, 1>::Zero();
-            return;
-        }
-    }
-
-    Eigen::Vector2d p0, p1;
-    Eigen::Vector4d q0, q1;
-    Eigen::Vector3d r0, r1;
-
-    p0 = {points[0].x, points[0].y};
-    p1 = {points[1].x, points[1].y};
-
-    cams_[0].unproject(p0, q0);
-    cams_[1].unproject(p1, q1);
-
-    r0 = q0.head(3), r1 = q1.head(3);
-
-    // param: cam0 unit vector, cam1 unit vector, transform from cam1 to cam0;
-    worldPoint = triangulate(r0, r1, T_cn_cnm1.inverse());
-
-    std::cout << "pixel coord:\n";
-    std::cout << p0.transpose() << std::endl << p1.transpose() << std::endl;
-
-    std::cout << "cam coord unit vector:\n";
-    std::cout << q0.transpose() << std::endl << q1.transpose() << std::endl;
-
-    std::cout << "unit_vector (x,y,z), distance:\n";
-    std::cout << worldPoint.head(3).transpose() << " " << 1/worldPoint(3) << std::endl;
-
-    std::cout << "actual coordinates:\n";
-    std::cout << worldPoint.head(3).transpose() / worldPoint(3) <<std::endl<<std::endl;
-    
-}
-
-/// Triangulates the point and returns homogenous representation. First 3
-/// components - unit-length direction vector. Last component inverse
-/// distance.
-template <class Derived>
-Eigen::Matrix<typename Derived::Scalar, 4, 1> DepthVerification::triangulate(
-    const Eigen::MatrixBase<Derived>& f0,
-    const Eigen::MatrixBase<Derived>& f1,
-    const Sophus::SE3<typename Derived::Scalar>& T_0_1) {
-    
-    EIGEN_STATIC_ASSERT_VECTOR_SPECIFIC_SIZE(Derived, 3);
-
-    using Scalar = typename Derived::Scalar;
-    using Vec4 = Eigen::Matrix<Scalar, 4, 1>;
-
-    Eigen::Matrix<Scalar, 3, 4> P1, P2;
-    P1.setIdentity();
-    P2 = T_0_1.inverse().matrix3x4();
-
-    Eigen::Matrix<Scalar, 4, 4> A(4, 4);
-    A.row(0) = f0[0] * P1.row(2) - f0[2] * P1.row(0);
-    A.row(1) = f0[1] * P1.row(2) - f0[2] * P1.row(1);
-    A.row(2) = f1[0] * P2.row(2) - f1[2] * P2.row(0);
-    A.row(3) = f1[1] * P2.row(2) - f1[2] * P2.row(1);
-
-    Eigen::JacobiSVD<Eigen::Matrix<Scalar, 4, 4>> mySVD(A, Eigen::ComputeFullV);
-    Vec4 worldPoint = mySVD.matrixV().col(3);
-    worldPoint /= worldPoint.template head<3>().norm();
-
-    // Enforce same direction of bearing vector and initial point
-    if (f0.dot(worldPoint.template head<3>()) < 0) worldPoint *= -1;
-
-    return worldPoint;
-  }
-
--- src/depth_verification_test.cpp
+++ src/depth_verification_test.cpp
@@ -6,25 +6,26 @@
 
 int main(){
 
-    std::string imgl, imgr;
-    std::ifstream f;
+    // std::string imgl, imgr;
+    // std::ifstream f;
 
-    f.open("../depth_verification_data/images.txt");
-    
-    if(!f.is_open()){
-        std::cout << "Error opening image txt file" << std::endl;
-        exit(1);
-    }
+    // f.open("../depth_verification_data/images.txt");
+    // if(!f.is_open()){
+    //     std::cout << "Error opening image txt file" << std::endl;
+    //     exit(1);
+    // }
+    // f >> imgl >> imgr;
+    // f.close();
 
-    f >> imgl >> imgr;
-    f.close();
+    // std::vector<std::string> imageFileNames = {"../depth_verification_data/" + imgl, "../depth_verification_data/" + imgr};
 
-    std::vector<std::string> imageFileNames = {"../depth_verification_data/" + imgl, "../depth_verification_data/" + imgr};
-    // std::vector<std::string> imageFileNames = {"../depth_verification_data/image1.png", "../depth_verification_data/image2.png"};
+    std::vector<std::string> imageFileNames = {
+        "../depth_verification_data/fisheye1-1596704736.954.png", 
+        "../depth_verification_data/fisheye2-1596704736.954.png"};
 
     DepthVerification dv;           
-    dv.parseYamlConfig("../depth_verification_data/t265-ds-none.yaml");
     dv.readImageFiles(imageFileNames);
+    dv.parseYamlConfig("../depth_verification_data/t265-omni-radtan.yaml");
     dv.start();
 
 
